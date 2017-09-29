46bec4d22e98725abf11d64f8aa30ba6816104a3
Ryou Zhang <peace_of_heaven@hotmail.com>
2017-09-28 22:51:56 -0500

    Create sample.go

---
+++ sample.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"time"
+	"fmt"
+	"github.com/RyouZhang/async-go"
+)
+
+func test_func(x, y int) int {
+	<-time.After(1  * time.Second)
+	return x + y
+}
+
+func test_func2(x, y int) int {
+	<-time.After(2  * time.Second)
+	return x * y	
+}
+
+func main() {
+
+	var (
+		res interface{}
+		err error
+	)
+
+	f := func() (interface{}, error) {
+		z := test_func(2, 3)
+		return z, nil
+	}
+	//working well
+	res, err = async.Lambda(f, 5 * time.Second)
+	fmt.Println(res, err)
+	//now timeout trigger
+	res, err = async.Lambda(f, 50 * time.Millisecond)
+	fmt.Println(res, err)
+	
+	f2 := func() (interface{}, error) {
+		z := test_func2(2, 3)
+		return z, nil
+	}
+
+	//for parallel
+	res = async.All([]async.LambdaMethod{f, f2}, 2 * time.Second)
+	fmt.Println(res)
+
+	res = async.All([]async.LambdaMethod{f, f2}, 5 * time.Second)
+	fmt.Println(res)
+
+	//for any, one error all error
+	res, err = async.Any([]async.LambdaMethod{f, f2}, 2 * time.Second)
+	fmt.Println(res, err)
+}
